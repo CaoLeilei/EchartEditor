1038c1ef04395125baecf8b21861b6fcee6f0772
曹思源 <ll900618@163.com>
2019-04-02 01:01:02 +0800

    111

---
+++ src/main/java/com/sean/echartoptions/EchartOptionsApplication.java
@@ -0,0 +1,13 @@
+package com.sean.echartoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchartOptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchartOptionsApplication.class, args);
+    }
+
+}
